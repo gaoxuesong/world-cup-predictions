4b299ad6fe95e581f5687037e7bc06f730bbdf03
ynouri <ynouri@users.noreply.github.com>
2018-06-10 17:17:23 -0400

    Print game results

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 
 from worldcup import WorldCup
+from gamepredictor import HistoricalGamePredictor
+
 
 if __name__ == "__main__":
-    wc2018 = WorldCup("groups.csv")
+
+    # Create World Cup 2018 object from groups file
+    wc_2018 = WorldCup("groups.csv")
+
+    # Create game predictor based on historical results
+    hist_gp = HistoricalGamePredictor("results.csv")
+
+    # Run predictions for the group stage
+    wc_2018.play(hist_gp)
+
+    # Print all game predictions
+    wc_2018.print_game_results()
